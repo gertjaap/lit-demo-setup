bf3ca45a3569f7e0eec8adda3832d2a058c61904
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-10-09 14:09:17 +0200

    Auto restart container on crash or restart

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker stop litdemoadminpanel
 docker rm litdemoadminpanel
 set -e
 
-docker run -d -e NODECOUNT=100 -e LITWEBUI=http://localhost:8999/ -p 8000:8000 -v "$PWD/data:/data" -v /var/run/docker.sock:/var/run/docker.sock --name litdemoadminpanel adminpanel
+docker run -d --restart always -e NODECOUNT=100 -e LITWEBUI=http://localhost:8999/ -p 8000:8000 -v "$PWD/data:/data" -v /var/run/docker.sock:/var/run/docker.sock --name litdemoadminpanel adminpanel
