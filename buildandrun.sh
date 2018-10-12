3e4456e1099c49b0492797e43857bab3106bbe39
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-10-12 14:28:28 +0200

    Removed demo ui from build script

--- buildandrun.sh
+++ buildandrun.sh
@@ -11,12 +11,6 @@ npm run build
 rm -rf ../admin-api/static/admin
 mv build ../admin-api/static
 mv ../admin-api/static/build ../admin-api/static/admin 
-cd ../demo-ui
-npm install
-npm run build
-rm -rf ../admin-api/static/demo
-mv build ../admin-api/static
-mv ../admin-api/static/build ../admin-api/static/demo 
 cd ..
 
 ./buildcontainerandrun.sh
